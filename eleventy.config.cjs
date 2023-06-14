650c513bf9827a396f36d093e6d01745d14ce47d
Chris Zuber <shgysk8zer0@gmail.com>
2023-06-14 14:47:04 -0700

    Update 11ty config file

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,22 +1,21 @@
+#!/usr/bin/env node
 /* eslint-env node */
 const { load } = require('js-yaml');
 const filters = require('@shgysk8zer0/11ty-filters');
+const { markdownIt } = require('@shgysk8zer0/11ty-netlify/markdown');
 const { importmap } = require('@shgysk8zer0/importmap');
-const { markdownIt } = require('@shgysk8zer0/11ty-netlify/markdown.cjs');
 
 module.exports = function(eleventyConfig) {
 	Object.entries(filters).forEach(([filter, cb]) => eleventyConfig.addFilter(filter, cb));
-
-	eleventyConfig.addFilter('is_icon', function(list) {
-		console.log(Object.getOwnPropertyDescriptors(this.eleventy));
+	eleventyConfig.addFilter('trim', input => input.trim());
+	eleventyConfig.addFilter('time', input => new Date(`2000-01-01T${input}`).toLocaleTimeString());
+	eleventyConfig.addFilter('is_icon', list => {
 		return JSON.stringify(list.filter(icon => typeof icon.purpose === 'string'));
 	});
 
 	// Add `_data/*.yml` & `_data/*.yaml` parsing as data files
 	eleventyConfig.addDataExtension('yaml', contents => load(contents));
 	eleventyConfig.addDataExtension('yml', contents => load(contents));
-
-	eleventyConfig.setLibrary('md', markdownIt);
 
 	// These directories get copied to `_site/`
 	eleventyConfig.addPassthroughCopy('js');
@@ -29,9 +28,11 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addLayoutAlias('post', '11ty-layouts/post.html');
 	eleventyConfig.addLayoutAlias('default', '11ty-layouts/default.html');
 
-	eleventyConfig.addGlobalData('importmap', importmap);
+	eleventyConfig.setLibrary('md', markdownIt);
+
 	// Set global data/variables
 	// {{ environment }} -> 'production' | 'development'
+	eleventyConfig.addGlobalData('importmap', importmap);
 	eleventyConfig.addGlobalData('environment',
 		process.env.ELEVENTY_RUN_MODE === 'build'
 			? 'production'
@@ -50,7 +51,7 @@ module.exports = function(eleventyConfig) {
 			dynamicPartials: false,
 			jekyllInclude: true,
 			extname: '.html',
-			foo: 'bar'
 		}
 	};
 };
+
