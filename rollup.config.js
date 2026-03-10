b5d43acaedd30af3930a04663c0bbbe92d5c5912
Chris Zuber <shgysk8zer0@gmail.com>
2026-03-10 12:21:54 -0700

    Config updates

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,12 @@
 import { getConfig } from '@shgysk8zer0/js-utils/rollup';
 import { rollupImport, rollupImportMeta } from '@shgysk8zer0/rollup-import';
 import { importmap } from '@shgysk8zer0/importmap';
+import pkg from './package.json' with { type: 'json' };
 
 export default getConfig('./js/index.js', {
 	plugins: [
 		rollupImport(importmap),
-		rollupImportMeta({ baseURL: 'https://krvbridge.org/' }),
+		rollupImportMeta({ baseURL: pkg.homepage }),
 	],
 	format: 'iife',
 	minify: true,
