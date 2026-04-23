af88ece25c6eabd6f447ad633d576b1a14fdb79b
Chris Zuber <shgysk8zer0@gmail.com>
2026-04-23 11:14:02 -0700

    Update Trusted Types

--- js/index.js
+++ js/index.js
@@ -3,9 +3,34 @@ import { getGooglePolicy } from '@shgysk8zer0/kazoo/trust-policies.js';
 import { toggleClass, on } from '@shgysk8zer0/kazoo/dom.js';
 import { init } from '@shgysk8zer0/kazoo/data-handlers.js';
 import { importGa, externalHandler, telHandler, mailtoHandler } from '@shgysk8zer0/kazoo/google-analytics.js';
+import { registerServiceWorker } from '@aegisjsproject/hermes/registry.js';
 import { submitHandler } from './contact-demo.js';
 import { GA } from './consts.js';
 import './components.js';
+
+if ('trustedTypes' in globalThis) {
+	trustedTypes.createPolicy('default', {
+		createHTML() {
+			return trustedTypes.emptyHTML;
+		},
+		createScript() {
+			return trustedTypes.emptyScript;
+		},
+		createScriptURL(input) {
+			const url = URL.parse(input, document.baseURI);
+
+			if (url instanceof URL && url.origin === location.origin) {
+				return url.href;
+			} else {
+				throw new TypeError(`Invalid script URL "${input}".`);
+			}
+		}
+	});
+}
+
+registerServiceWorker(document.documentElement.dataset.serviceWorker, {
+	type: document.documentElement.dataset.serviceWorkerType ?? 'module',
+});
 
 toggleClass([document.documentElement], {
 	'no-dialog': document.createElement('dialog') instanceof HTMLUnknownElement,
